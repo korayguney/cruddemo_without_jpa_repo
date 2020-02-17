3c7124b6296c5a9b18c3df125a1a5015cac8a2a2
Koray Guney <kguney@netrd.com.tr>
2020-02-17 16:02:32 +0300

    added spring boot data rest support

--- Downloads/cruddemo/src/main/java/com/koray/cruddemo/dao/EmployeeJpaRepository.java
+++ Downloads/cruddemo/src/main/java/com/koray/cruddemo/dao/EmployeeJpaRepository.java
@@ -2,7 +2,9 @@ package com.koray.cruddemo.dao;
 
 import com.koray.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Integer> {
 
 }
